88a929668207a7e1c9ef291d35b6298eafd20bf4
ioannismavromatis <ioan.mavromatis@gmail.com>
2020-10-18 20:03:10 +0100

    Style fixes and shadows for buttons

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,4 +5,17 @@ $(document).ready(function() {
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
+
+    $('a.copybutton').click(function (event) {
+        var buttonid = event.target.id;
+        var range = document.createRange();
+        var padid = "copy-";
+        var newid = padid.concat(buttonid);
+
+        var range = document.createRange();
+        range.selectNode(document.getElementById(newid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();// to deselect
+    });
 });
